54b590d3595cdcf1f066ad888ef014116b955158
Juan Pablo Chica <juanpchica@hotmail.com>
2021-04-14 14:23:24 -0500

    New schema moongose added and database configured

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose'); //Connect mongodb easy
 
 require('dotenv').config();
 
@@ -8,6 +9,15 @@ const port  = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+//Mongo connection 
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser:true, useCreateIndex:true,useUnifiedTopology: true});
+
+const connection = mongoose.connection;
+connection.once('open',()=>{
+    console.log("MongoDB database connection established successfully")
+})
 
 app.listen(port, ()=>{
     console.log(`Server is running on port: ${port}`);
